70376624e496144f350a86d56ad37d4d40d547fa
Andrew Hefley Carpenter <drewwcarpenter@gmail.com>
2014-12-09 14:48:17 -0500

    revise, refactor, tidy up

--- models/listing.rb
+++ models/listing.rb
@@ -11,13 +11,11 @@ class Listing
   def self.get_corpus(response, comments = [])
     response.each do |key,value|
       value = value || key
-
-      if value.is_a?(Hash) || value.is_a?(Array)
+      
+	  if value.is_a?(Hash) || value.is_a?(Array)
         get_corpus(value, comments)
-      else
-        if key == 'body'
-          comments << (value.gsub(/[\n\t\r]/, ' ') + ' ')
-        end
+      elsif key == 'body'
+        comments << value.gsub(/[\n\t\r]/, ' ') + ' '
       end
     end
       
@@ -28,16 +26,12 @@ class Listing
     num_of_comments = reddit.get_comments(subreddit: 'AskReddit', link_id: '2lrb1w')[0]["data"]["children"][0]["data"]["num_comments"]
 	documents = RubyTfIdf::TfIdf.new(get_corpus(reddit.get_comments(subreddit: subreddit, link_id: link_id, limit: num_of_comments, depth: num_of_comments)[1]["data"]), 3, true).tf_idf #.delete_if {|element| element.empty?}.uniq {|element| element.first[0]}.sort{|element| element.first[1]}
 	words = {}
-	for document in documents
-	  for word in document
-	    if words.has_key? word[0]
-		  words[word[0]] = words[word[0]] + word[1]
-		else
-		  words[word[0]] = word[1]
-		end
-     end
-   end
+	documents.each {|document|
+	  document.each {|word|
+	    words.has_key?(word[0]) ? words[word[0]] = words[word[0]] + word[1] : words[word[0]] = word[1]
+     }
+   }
    
-   words #.sort{|x, y| y[1] <=> x[1]}[1..200].to_h 
+   words
   end  
 end
